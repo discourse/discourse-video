c6b5705fe143ceaa4d854b162c772dff3ec90323
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2022-05-23 14:16:19 +0200

    linting

--- assets/javascripts/discourse/controllers/discourse-video-upload-modal.js
+++ assets/javascripts/discourse/controllers/discourse-video-upload-modal.js
@@ -1,13 +1,5 @@
 import Controller from "@ember/controller";
-import ChatChannel from "discourse/plugins/discourse-chat/discourse/models/chat-channel";
-import discourseComputed from "discourse-common/utils/decorators";
-import escape from "discourse-common/lib/escape";
-import I18n from "I18n";
 import ModalFunctionality from "discourse/mixins/modal-functionality";
-import { ajax } from "discourse/lib/ajax";
-import { action } from "@ember/object";
-import { inject as service } from "@ember/service";
-import { isBlank } from "@ember/utils";
 
 export default Controller.extend(ModalFunctionality, {
   afterUploadComplete: null,
