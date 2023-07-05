702a32f7f715539664de1b5807f12c45aa01125e
Bianca Nenciu <nenciu.bianca@gmail.com>
2023-07-05 21:39:22 +0300

    DEV: Call requires_plugin with plugin name

--- lib/discourse_video/engine.rb
+++ lib/discourse_video/engine.rb
@@ -1,10 +1,11 @@
 # frozen_string_literal: true
 
 module DiscourseVideo
+  PLUGIN_NAME = "discourse-video"
   POST_CUSTOM_FIELD_NAME = "discourse_video"
 
   class Engine < ::Rails::Engine
-    engine_name "DiscourseVideo"
+    engine_name PLUGIN_NAME
     isolate_namespace DiscourseVideo
 
     config.after_initialize do
