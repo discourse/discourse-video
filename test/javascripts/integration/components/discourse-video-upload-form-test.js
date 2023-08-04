ccb859f3041fd5c6b6539c477b30c593472022c7
Blake Erickson <o.blakeerickson@gmail.com>
2023-08-04 17:14:29 -0600

    lint tests

--- test/javascripts/integration/components/discourse-video-upload-form-test.js
+++ test/javascripts/integration/components/discourse-video-upload-form-test.js
@@ -14,17 +14,20 @@ discourseModule(
       template: hbs`<DiscourseVideoUploadForm/>`,
 
       test(assert) {
-        const component = this.owner.lookup('component:discourse-video-upload-form');
-        assert.strictEqual(component.durationMinutes(60),1);
+        const component = this.owner.lookup(
+          "component:discourse-video-upload-form"
+        );
+        assert.strictEqual(component.durationMinutes(60), 1);
       },
-
     });
 
     componentTest("Staff do not have a duration limit", {
       template: hbs`<DiscourseVideoUploadForm/>`,
 
       test(assert) {
-        const component = getOwner(this).lookup('component:discourse-video-upload-form');
+        const component = getOwner(this).lookup(
+          "component:discourse-video-upload-form"
+        );
 
         component.setProperties({
           currentUser: { staff: true },
@@ -33,15 +36,16 @@ discourseModule(
         });
 
         assert.strictEqual(component.isDurationAllowed(), true);
-      }
-
+      },
     });
 
     componentTest("Trust level 1 users have a duration limit", {
       template: hbs`<DiscourseVideoUploadForm/>`,
 
       test(assert) {
-        const component = getOwner(this).lookup('component:discourse-video-upload-form');
+        const component = getOwner(this).lookup(
+          "component:discourse-video-upload-form"
+        );
 
         component.setProperties({
           currentUser: { staff: false, trust_level: 1 },
@@ -50,32 +54,37 @@ discourseModule(
         });
 
         assert.strictEqual(component.isDurationAllowed(), false);
-      }
-
+      },
     });
 
-    componentTest("Trust level 1 users can upload if under the duration limit", {
-      template: hbs`<DiscourseVideoUploadForm/>`,
+    componentTest(
+      "Trust level 1 users can upload if under the duration limit",
+      {
+        template: hbs`<DiscourseVideoUploadForm/>`,
 
-      test(assert) {
-        const component = getOwner(this).lookup('component:discourse-video-upload-form');
+        test(assert) {
+          const component = getOwner(this).lookup(
+            "component:discourse-video-upload-form"
+          );
 
-        component.setProperties({
-          currentUser: { staff: false, trust_level: 1 },
-          videoDurationMinutes: 4,
-          siteSettings: { discourse_video_max_duration_minutes: 5 },
-        });
+          component.setProperties({
+            currentUser: { staff: false, trust_level: 1 },
+            videoDurationMinutes: 4,
+            siteSettings: { discourse_video_max_duration_minutes: 5 },
+          });
 
-        assert.strictEqual(component.isDurationAllowed(), true);
+          assert.strictEqual(component.isDurationAllowed(), true);
+        },
       }
-
-    });
+    );
 
     componentTest("Trust level 4 users have a duration limit", {
       template: hbs`<DiscourseVideoUploadForm/>`,
 
       test(assert) {
-        const component = getOwner(this).lookup('component:discourse-video-upload-form');
+        const component = getOwner(this).lookup(
+          "component:discourse-video-upload-form"
+        );
 
         component.setProperties({
           currentUser: { staff: false, trust_level: 4 },
@@ -84,33 +93,38 @@ discourseModule(
         });
 
         assert.strictEqual(component.isDurationAllowed(), false);
-      }
-
+      },
     });
 
-    componentTest("Trust level 4 users can upload if under the duration limit", {
-      template: hbs`<DiscourseVideoUploadForm/>`,
+    componentTest(
+      "Trust level 4 users can upload if under the duration limit",
+      {
+        template: hbs`<DiscourseVideoUploadForm/>`,
 
-      test(assert) {
-        const component = getOwner(this).lookup('component:discourse-video-upload-form');
+        test(assert) {
+          const component = getOwner(this).lookup(
+            "component:discourse-video-upload-form"
+          );
 
-        component.setProperties({
-          currentUser: { staff: false, trust_level: 4 },
-          videoDurationMinutes: 4,
-          siteSettings: { discourse_video_max_duration_minutes_leaders: 5 },
-        });
+          component.setProperties({
+            currentUser: { staff: false, trust_level: 4 },
+            videoDurationMinutes: 4,
+            siteSettings: { discourse_video_max_duration_minutes_leaders: 5 },
+          });
 
-        assert.strictEqual(component.isDurationAllowed(), true);
-        assert.strictEqual(component.maxVideoDurationMinutes, 5);
+          assert.strictEqual(component.isDurationAllowed(), true);
+          assert.strictEqual(component.maxVideoDurationMinutes, 5);
+        },
       }
-
-    });
+    );
 
     componentTest("Returns false if the duration could not be calculated", {
       template: hbs`<DiscourseVideoUploadForm/>`,
 
       test(assert) {
-        const component = getOwner(this).lookup('component:discourse-video-upload-form');
+        const component = getOwner(this).lookup(
+          "component:discourse-video-upload-form"
+        );
 
         component.setProperties({
           currentUser: { staff: false, trust_level: 4 },
@@ -120,8 +134,7 @@ discourseModule(
 
         assert.strictEqual(component.isDurationAllowed(), false);
         assert.strictEqual(component.maxVideoDurationMinutes, null);
-      }
-
+      },
     });
   }
 );
