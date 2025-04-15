discourse/discourse-video

commit 54718f50d783491fa1ae3683064556effd5ec6e2

DEV: Update modal to Component API (#52)
- update modal to Component API
- update ci workflow
- updating linting config 
- lint

assets/javascripts/discourse/components/modal/discourse-video-upload-form.gjs
@@ -1,0 +1,234 @@
+import Component from "@glimmer/component";
+import { tracked } from "@glimmer/tracking";
+import { action } from "@ember/object";
+import { inject as service } from "@ember/service";
+import { Promise } from "rsvp";
+import DButton from "discourse/components/d-button";
+import DModal from "discourse/components/d-modal";
+import { ajax } from "discourse/lib/ajax";
+import { popupAjaxError } from "discourse/lib/ajax-error";
+import dIcon from "discourse-common/helpers/d-icon";
+import i18n from "discourse-common/helpers/i18n";
+import I18n from "discourse-i18n";
+import not from "truth-helpers/helpers/not";
+
+const UPCHUNK = window.UpChunk;
+
+export default class DiscourseVideoUploadForm extends Component {
+  @service dialog;
+  @service appEvents;
+  @service siteSettings;
+  @service currentUser;
+
+  @tracked file = this.args.model?.file || null;
+  @tracked videoDurationMinutes = null;
+  @tracked uploadProgress;
+  @tracked videoInfo;
+  @tracked uploading = false;
+
+  afterUploadComplete = this.args.model?.afterUploadComplete || null;
+
+  get fileSize() {
+    return this.humanFilesize(this.file.size);
+  }
+
+  get isDurationAllowed() {
+    if (this.currentUser.staff || this.videoDurationMinutes === null) {
+      return true;
+    }
+
+    return (
+      this.videoDurationMinutes < this.maxVideoDurationMinutes &&
+      this.currentUser.trust_level === 4
+    );
+  }
+
+  get maxVideoDurationMinutes() {
+    return this.currentUser.trust_level === 4
+      ? this.siteSettings.discourse_video_max_duration_minutes_leaders
+      : this.siteSettings.discourse_video_max_duration_minutes;
+  }
+
+  @action
+  async fileChanged(event) {
+    const file = event.target.files[0];
+    this.file = file;
+    const duration = await this.getVideoDuration(file);
+    this.videoDurationMinutes = this.durationMinutes(duration);
+  }
+
+  @action
+  upload() {
+    if (this.isAuthorizedVideo(this.file.name) && this.file.size > 0) {
+      this.isDurationAllowed
+        ? this.createVideoObject()
+        : this.dialog.alert(
+            !this.maxVideoDurationMinutes
+              ? I18n.t("discourse_video.post.errors.duration_error")
+              : I18n.t(
+                  "discourse_video.post.errors.allowed_duration_exceeded",
+                  {
+                    allowed_duration: this.maxVideoDurationMinutes,
+                  }
+                )
+          );
+    } else {
+      this.dialog.alert(
+        I18n.t("discourse_video.post.errors.upload_not_authorized", {
+          authorized_extensions: this.videoExtensionsToArray().join(", "),
+        })
+      );
+    }
+  }
+
+  @action
+  async getVideoDuration(file) {
+    return new Promise((resolve, reject) => {
+      let video = document.createElement("video");
+      video.preload = "metadata";
+
+      video.onloadedmetadata = () => {
+        resolve(video.duration);
+      };
+
+      video.onerror = () => {
+        reject("Error processing video file");
+      };
+
+      video.src = URL.createObjectURL(file);
+    });
+  }
+
+  @action
+  async createVideoObject() {
+    try {
+      this.uploading = true;
+      this.setProgress("preparing");
+      const videoInfo = await ajax("/discourse_video/create", {
+        type: "POST",
+        data: { filename: this.file.name },
+      });
+      this.setupUpChunk(videoInfo);
+    } catch (reason) {
+      this.uploading = false;
+      this.setProgress("error");
+      popupAjaxError(reason);
+    }
+  }
+
+  setProgress(key, args) {
+    this.uploadProgress = I18n.t(
+      `discourse_video.upload_progress.${key}`,
+      args
+    );
+  }
+
+  setupUpChunk(videoInfo) {
+    this.setProgress("starting");
+    this.videoInfo = videoInfo;
+
+    const upload = UPCHUNK.createUpload({
+      endpoint: videoInfo["api_request_url"],
+      file: this.file,
+      chunkSize: 5120, // Uploads the file in ~5mb chunks
+    });
+
+    upload.on("progress", (progress) => {
+      this.setProgress("uploading", {
+        progress: progress.detail.toFixed(1),
+      });
+    });
+
+    upload.on("success", () => {
+      this.uploadComplete();
+    });
+  }
+
+  uploadComplete() {
+    const videoInfo = this.videoInfo;
+    this.setProgress("complete", { info: `[video=${videoInfo["video_id"]}]` });
+    let videoTag = `[video=${videoInfo["video_id"]}]`;
+    if (this.siteSettings.discourse_video_enable_mp4_download === true) {
+      videoTag += ` [download-video=${videoInfo["video_id"]}]`;
+    }
+
+    if (this.afterUploadComplete) {
+      this.afterUploadComplete(videoTag);
+    } else {
+      this.appEvents.trigger("composer:insert-text", videoTag);
+    }
+    this.args.closeModal();
+  }
+
+  videoExtensionsToArray() {
+    return this.siteSettings.discourse_video_file_extensions
+      .toLowerCase()
+      .replace(/[\s\.]+/g, "")
+      .split("|")
+      .filter((ext) => !ext.includes("*"));
+  }
+
+  isAuthorizedVideo(fileName) {
+    return new RegExp(
+      `\\.(${this.videoExtensionsToArray().join("|")})$`,
+      "i"
+    ).test(fileName);
+  }
+
+  durationMinutes(duration) {
+    return parseInt(duration / 60, 10);
+  }
+
+  humanFilesize(size) {
+    let i = size === 0 ? 0 : Math.floor(Math.log(size) / Math.log(1024));
+    return (
+      (size / Math.pow(1024, i)).toFixed(2) * 1 +
+      " " +
+      ["B", "kB", "MB", "GB", "TB"][i]
+    );
+  }
+
+  <template>
+    <DModal
+      @title={{i18n "discourse_video.modal_title"}}
+      @subtitle={{i18n "discourse_video.modal_subtitle"}}
+      class="discourse-video-upload-modal"
+      @closeModal={{@closeModal}}
+    >
+      <:body>
+        <h3>{{i18n "discourse_video.file"}}</h3>
+        <p>
+          {{#if this.file}}
+            {{this.file.name}}
+            ({{this.fileSize}})
+          {{/if}}
+          <label
+            class="btn"
+            disabled={{this.uploading}}
+            title={{i18n "discourse_video.select_file"}}
+          >
+            {{dIcon "video"}}&nbsp;{{i18n "discourse_video.select_file"}}
+            <input
+              disabled={{this.uploading}}
+              type="file"
+              id="video-upload-input"
+              onchange={{this.fileChanged}}
+            />
+          </label>
+        </p>
+      </:body>
+      <:footer>
+        {{#if this.uploading}}
+          {{this.uploadProgress}}
+        {{else}}
+          <DButton
+            @action={{this.upload}}
+            @icon="upload"
+            @label="upload"
+            @disabled={{not this.file}}
+          />
+        {{/if}}
+      </:footer>
+    </DModal>
+  </template>
+}

.eslintrc
@@ -1,4 +1,0 @@
-{
-  "extends": "eslint-config-discourse",
-  "ignorePatterns": ["/assets/vendor", "/public"]
-}

.eslintrc.cjs
@@ -1,0 +1,1 @@
+module.exports = require("@discourse/lint-configs/eslint");

.github/workflows/plugin-linting.yml
@@ -1,52 +1,0 @@
-name: Linting
-
-on:
-  push:
-    branches:
-      - master
-      - main
-  pull_request:
-
-jobs:
-  build:
-    runs-on: ubuntu-latest
-
-    steps:
-      - uses: actions/checkout@v2
-
-      - name: Set up Node.js
-        uses: actions/setup-node@v1
-        with:
-          node-version: 12
-
-      - name: Set up ruby
-        uses: ruby/setup-ruby@v1
-        with:
-          ruby-version: 2.7
-          bundler-cache: true
-
-      - name: Yarn install
-        run: yarn install
-
-      - name: ESLint
-        if: ${{ always() }}
-        run: yarn eslint --ext .js,.js.es6 --no-error-on-unmatched-pattern {test,assets}/javascripts
-
-      - name: Prettier
-        if: ${{ always() }}
-        shell: bash
-        run: |
-          yarn prettier -v
-          if [ 0 -lt $(find assets -type f \( -name "*.scss" -or -name "*.js" -or -name "*.es6" \) 2> /dev/null | wc -l) ]; then
-            yarn prettier --list-different "assets/**/*.{scss,js,es6}"
-          fi
-          if [ 0 -lt $(find test -type f \( -name "*.js" -or -name "*.es6" \) 2> /dev/null | wc -l) ]; then
-            yarn prettier --list-different "test/**/*.{js,es6}"
-          fi
-      - name: Ember template lint
-        if: ${{ always() }}
-        run: yarn ember-template-lint assets/javascripts
-
-      - name: Rubocop
-        if: ${{ always() }}
-        run: bundle exec rubocop .

.github/workflows/plugin-tests.yml
@@ -1,126 +1,11 @@
-name: Plugin Tests
+name: Discourse Plugin
 
 on:
   push:
     branches:
-      - master
       - main
   pull_request:
 
 jobs:
-  build:
-    name: ${{ matrix.build_type }}
-    runs-on: ubuntu-latest
-    container: discourse/discourse_test:release
-    timeout-minutes: 60
-
-    env:
-      DISCOURSE_HOSTNAME: www.example.com
-      RUBY_GLOBAL_METHOD_CACHE_SIZE: 131072
-      RAILS_ENV: test
-      PGHOST: postgres
-      PGUSER: discourse
-      PGPASSWORD: discourse
-
-    strategy:
-      fail-fast: false
-
-      matrix:
-        build_type: ["backend", "frontend"]
-        ruby: ["2.7"]
-        postgres: ["12"]
-
-    services:
-      postgres:
-        image: postgres:${{ matrix.postgres }}
-        ports:
-          - 5432:5432
-        env:
-          POSTGRES_USER: discourse
-          POSTGRES_PASSWORD: discourse
-        options: >-
-          --mount type=tmpfs,destination=/var/lib/postgresql/data
-          --health-cmd pg_isready
-          --health-interval 10s
-          --health-timeout 5s
-          --health-retries 5
-    steps:
-      - uses: actions/checkout@v2
-        with:
-          repository: discourse/discourse
-          fetch-depth: 1
-
-      - name: Install plugin
-        uses: actions/checkout@v2
-        with:
-          path: plugins/${{ github.event.repository.name }}
-          fetch-depth: 1
-
-      - name: Setup Git
-        run: |
-          git config --global user.email "ci@ci.invalid"
-          git config --global user.name "Discourse CI"
-
-      - name: Start redis
-        run: |
-          redis-server /etc/redis/redis.conf &
-
-      - name: Bundler cache
-        uses: actions/cache@v2
-        with:
-          path: vendor/bundle
-          key: ${{ runner.os }}-${{ matrix.ruby }}-gem-${{ hashFiles('**/Gemfile.lock') }}
-          restore-keys: |
-            ${{ runner.os }}-${{ matrix.ruby }}-gem-
-      - name: Setup gems
-        run: |
-          bundle config --local path vendor/bundle
-          bundle config --local deployment true
-          bundle config --local without development
-          bundle install --jobs 4
-          bundle clean
-      - name: Lint English locale
-        if: matrix.build_type == 'backend'
-        run: bundle exec ruby script/i18n_lint.rb "plugins/${{ github.event.repository.name }}/locales/{client,server}.en.yml"
-
-      - name: Get yarn cache directory
-        id: yarn-cache-dir
-        run: echo "::set-output name=dir::$(yarn cache dir)"
-
-      - name: Yarn cache
-        uses: actions/cache@v2
-        id: yarn-cache
-        with:
-          path: ${{ steps.yarn-cache-dir.outputs.dir }}
-          key: ${{ runner.os }}-${{ matrix.os }}-yarn-${{ hashFiles('**/yarn.lock') }}
-          restore-keys: |
-            ${{ runner.os }}-${{ matrix.os }}-yarn-
-      - name: Yarn install
-        run: yarn install
-
-      - name: Migrate database
-        run: |
-          bin/rake db:create
-          bin/rake db:migrate
-      - name: Check spec existence
-        id: check_spec
-        shell: bash
-        run: |
-          if [ 0 -lt $(find plugins/${{ github.event.repository.name }}/spec -type f -name "*.rb" 2> /dev/null | wc -l) ]; then
-            echo "::set-output name=files_exist::true"
-          fi
-      - name: Plugin RSpec
-        if: matrix.build_type == 'backend' && steps.check_spec.outputs.files_exist == 'true'
-        run: bin/rake plugin:spec[${{ github.event.repository.name }}]
-
-      - name: Check qunit existence
-        id: check_qunit
-        shell: bash
-        run: |
-          if [ 0 -lt $(find plugins/${{ github.event.repository.name }}/test/javascripts -type f \( -name "*.js" -or -name "*.es6" \) 2> /dev/null | wc -l) ]; then
-            echo "::set-output name=files_exist::true"
-          fi
-      - name: Plugin QUnit
-        if: matrix.build_type == 'frontend' && steps.check_qunit.outputs.files_exist == 'true'
-        run: bundle exec rake plugin:qunit['${{ github.event.repository.name }}','1200000']
-        timeout-minutes: 30
+  ci:
+    uses: discourse/.github/.github/workflows/discourse-plugin.yml@v1

.prettierrc
@@ -1,1 +1,0 @@
-{}

.prettierrc.cjs
@@ -1,0 +1,1 @@
+module.exports = require("@discourse/lint-configs/prettier");

.rubocop.yml
@@ -1,2 +1,2 @@
 inherit_gem:
-  rubocop-discourse: default.yml
+  rubocop-discourse: stree-compat.yml

commit 88bce336c30ab616419acc4f93907ce6cfe962a4

FIX: Max video duration based on trust level
The logic for max video duration based on trust level was wrong and not
working for users less than trust level 4. This was a regression caused
by a refactoring in 54718f50d783491fa1ae3683064556effd5ec6e2.

This fix allows users with a trust level < 4 to upload videos as long as
they don't exceed the duration set by
`SiteSetting.discourse_video_max_duration_minutes`.

assets/javascripts/discourse/components/modal/discourse-video-upload-form.gjs
@@ -37,8 +37,7 @@ export default class DiscourseVideoUploadForm extends Component {
     }
 
     return (
-      this.videoDurationMinutes < this.maxVideoDurationMinutes &&
-      this.currentUser.trust_level === 4
+      this.videoDurationMinutes < this.maxVideoDurationMinutes
     );
   }
 