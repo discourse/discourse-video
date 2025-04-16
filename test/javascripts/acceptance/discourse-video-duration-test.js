1c14a1ddf4de5d27e84f0e24f798dcf863480414
Blake Erickson <blake@discourse.org>
2025-04-16 16:22:02 -0600

    linting

--- test/javascripts/acceptance/discourse-video-duration-test.js
+++ test/javascripts/acceptance/discourse-video-duration-test.js
@@ -1,27 +1,27 @@
-import { module, test } from "qunit";
 import { setupTest } from "ember-qunit";
+import { module, test } from "qunit";
 import DiscourseVideoUploadForm from "discourse/plugins/discourse-video/discourse/components/modal/discourse-video-upload-form";
 
-module("Unit | Component | discourse-video-upload-form", function(hooks) {
+module("Unit | Component | discourse-video-upload-form", function (hooks) {
   setupTest(hooks);
 
   const isDurationAllowedFn = Object.getOwnPropertyDescriptor(
     DiscourseVideoUploadForm.prototype,
-    'isDurationAllowed'
+    "isDurationAllowed"
   ).get;
 
   function testIsDurationAllowed(assert, params, expectedResult, message) {
     const context = {
       currentUser: { staff: params.isStaff || false },
       videoDurationMinutes: params.duration,
-      maxVideoDurationMinutes: params.maxDuration || 5
+      maxVideoDurationMinutes: params.maxDuration || 5,
     };
 
     const result = isDurationAllowedFn.call(context);
     assert.strictEqual(result, expectedResult, message);
   }
 
-  test("isDurationAllowed returns false when duration exceeds limits", function(assert) {
+  test("isDurationAllowed returns false when duration exceeds limits", function (assert) {
     testIsDurationAllowed(
       assert,
       { duration: 10, maxDuration: 5 },
@@ -30,7 +30,7 @@ module("Unit | Component | discourse-video-upload-form", function(hooks) {
     );
   });
 
-  test("isDurationAllowed returns true when duration does not exceed limits", function(assert) {
+  test("isDurationAllowed returns true when duration does not exceed limits", function (assert) {
     testIsDurationAllowed(
       assert,
       { duration: 2, maxDuration: 5 },
@@ -39,7 +39,7 @@ module("Unit | Component | discourse-video-upload-form", function(hooks) {
     );
   });
 
-  test("isDurationAllowed returns true for staff users regardless of duration", function(assert) {
+  test("isDurationAllowed returns true for staff users regardless of duration", function (assert) {
     testIsDurationAllowed(
       assert,
       { isStaff: true, duration: 10, maxDuration: 5 },
