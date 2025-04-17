4bc0e32fed0adaa261e4bfbf47a248384d562f98
Blake Erickson <blake@discourse.org>
2025-04-17 13:18:25 -0600

    DEV: Add tests for composer placeholder (#83)
    Tests that a video placeholder is rendered in the composer preview pane.
    
    Follow up to: #79

--- test/javascripts/acceptance/discourse-video-enabled-test.js
+++ test/javascripts/acceptance/discourse-video-enabled-test.js
@@ -1,4 +1,4 @@
-import { click, visit } from "@ember/test-helpers";
+import { click, fillIn, visit } from "@ember/test-helpers";
 import { test } from "qunit";
 import { clearToolbarCallbacks } from "discourse/components/d-editor";
 import { acceptance } from "discourse/tests/helpers/qunit-helpers";
@@ -9,6 +9,7 @@ acceptance("Discourse video enabled", function (needs) {
     discourse_video_enabled: true,
     discourse_video_file_extensions: "mp4|mov|wmv|avi|mkv|mpg|mpeg|ogg",
     discourse_video_min_trust_level: 1,
+    allow_uncategorized_topics: true,
   });
   needs.hooks.beforeEach(() => clearToolbarCallbacks());
 
@@ -31,5 +32,39 @@ acceptance("Discourse video enabled", function (needs) {
     assert
       .dom(".discourse-video-upload-modal label.btn")
       .exists("the upload button is present");
+  });
+
+  test("Shows video placeholder in composer", async function (assert) {
+    await visit("/");
+    await click("#create-topic");
+
+    // Get the composer element and set its value directly
+    // This is for just testing the placeholder so we don't need to
+    // go through the whole upload flow and mocking upload requests
+    // to mux.
+    await fillIn(".d-editor-input", "[video=8kDeYZD5hHftgpWaTHTcjUuxt8s5qkQq]");
+
+    // Check if the preview pane shows the video container
+    assert
+      .dom(".d-editor-preview .discourse-video-container")
+      .exists("video container appears in preview");
+
+    // Check if it has the correct video ID
+    assert
+      .dom(".d-editor-preview .discourse-video-container")
+      .hasAttribute(
+        "data-video-id",
+        "8kDeYZD5hHftgpWaTHTcjUuxt8s5qkQq",
+        "video container has the correct video ID"
+      );
+
+    // Check if the placeholder elements exist
+    assert
+      .dom(".d-editor-preview .onebox-placeholder-container")
+      .exists("placeholder container appears in preview");
+
+    assert
+      .dom(".d-editor-preview .placeholder-icon.video")
+      .exists("video placeholder icon appears in preview");
   });
 });
